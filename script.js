52d3c1f4e68e51763b266fe58d20427a8fd6162a
Kuatbek <example@gmail.com>
2022-09-28 23:21:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+"use strict";
+// const obj =  {
+//     a: 10
+// }
+
+// obj.a = 20;
+// console.log(obj);
+
+// const obj = {
+//     name: "John",
+//     age: 23,
+//     isMarried: true,
+// };
+
+// console.log(obj.isMarried);
+
+// let arr = ['plum', 'orange.jpg'];
+// console.log(arr[0]);
+
+// alert("Hello");
+
+// const result = confirm("Are u here ?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?");
+// console.log(answer + 4);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя ?', '');
+// answers[1] = prompt('Как ваше фамилия ?', '');
+// answers[2] = prompt('Сколько вам лет ?', '');
+
+// console.log(answers);
+
+// const user = "Jack";
+
+// alert(`Hi, ${user}`);
+
+// let incr = 10;
+// let decr = 5;
+
+// incr++;
+// decr++;
+
+// console.log(incr);
+// console.log(decr);
+
+const isChecked = true;
+const isClosed = true;
+
+console.log(isChecked || isClosed);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
